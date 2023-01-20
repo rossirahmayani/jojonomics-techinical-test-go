d801eab2c3c0a31b1f4036fadea13c4474fadeb6
rossirahmayani <rossi.rahmayani.rr@gmail.com>
2023-01-21 01:36:49 +0700

    init config

---
+++ go.mod
@@ -0,0 +1,12 @@
+module jojonomics-techinical-test-go
+
+go 1.15
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/segmentio/kafka-go v0.4.38 // indirect
+	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
+)
---
+++ misc/db/V01__init_db.sql
@@ -0,0 +1,42 @@
+-- tbl rekening
+CREATE TABLE public.tbl_rekening (
+    id serial NOT NULL,
+    norek varchar NOT NULL,
+    saldo numeric NOT NULL DEFAULT 0,
+    create_date timestamp with time zone NULL,
+    update_date timestamp with time zone NULL
+);
+
+ALTER TABLE public.tbl_rekening ADD CONSTRAINT tbl_rekening_id_pk PRIMARY KEY (id);
+
+-- tbl harga
+CREATE TABLE public.tbl_harga (
+    id serial4 NOT NULL,
+    harga_topup numeric NOT NULL,
+    harga_buyback numeric NOT NULL,
+    reff_id varchar NOT NULL,
+    is_active boolean NOT NULL,
+    admin_id varchar NOT NULL,
+    create_date timestamptz NULL,
+    update_date timestamptz NULL
+);
+
+ALTER TABLE public.tbl_harga ADD CONSTRAINT tbl_harga_id_pk PRIMARY KEY (id);
+
+--tbl transaksi
+CREATE TABLE public.tbl_transaksi (
+    id bigserial NOT NULL,
+    rekening_id bigint NOT NULL,
+    harga numeric NOT NULL,
+    gram numeric NOT NULL,
+    "type" varchar NOT NULL,
+    reff_id varchar NOT NULL,
+    saldo_akhir numeric NOT NULL,
+    transaction_date timestamp with time zone NOT NULL,
+    create_date timestamp with time zone NULL,
+    update_date timestamp with time zone NULL
+);
+
+ALTER TABLE public.tbl_transaksi ADD CONSTRAINT tbl_transaksi_id_pk PRIMARY KEY (id);
+ALTER TABLE public.tbl_transaksi ADD CONSTRAINT tbl_transaksi_rek_id_fk FOREIGN KEY (id) REFERENCES public.tbl_rekening(id);
+
