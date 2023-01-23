7b96ed0fb27347936a6a29e64bd5abf92e5f77b9
rossirahmayani <rossi.rahmayani.rr@gmail.com>
2023-01-23 16:27:32 +0700

    update kafka config
    update kafka config

--- config/kafka/kafka.go
+++ config/kafka/kafka.go
@@ -1,1 +1,41 @@
 package kafka
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func loadEnv()  {
+	godotenv.Load("misc/.env")
+}
+
+func ConnProducer() *kafka.Producer{
+	loadEnv()
+	kafkaServer := os.Getenv("KAFKA_HOST") + ":" + os.Getenv("KAFKA_PORT")
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafkaServer})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
+
+func ConnConsumer() *kafka.Consumer{
+	loadEnv()
+	kafkaServer := os.Getenv("KAFKA_HOST") + ":" + os.Getenv("KAFKA_PORT")
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": kafkaServer,
+		"group.id":          "group",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c
+
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module jojonomics-techinical-test-go
 go 1.15
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.7
-	github.com/segmentio/kafka-go v0.4.38 // indirect
 	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569
 )
